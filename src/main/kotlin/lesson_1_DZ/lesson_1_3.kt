7465a9fb6659becdc330c4461a89c4bb40abc160
tbukhantsova <tatyana.savenkova.82@mail.ru>
2024-08-18 16:25:54 +0300

    KS-1-3-DZ year and date of fly

---
+++ src/main/kotlin/lesson_1_DZ/lesson_1_3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1_DZ
+
+fun main() {
+    val yearOfFlyYuriGagarin: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(yearOfFlyYuriGagarin)
+    println(hour)
+    println(minute)
+
+    hour = "10:"
+    minute = "55"
+
+    print(hour)
+    print(minute)
+    
+}
