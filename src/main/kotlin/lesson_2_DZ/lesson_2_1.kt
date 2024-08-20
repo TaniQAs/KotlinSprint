262280780365cd6bd5142e1e549d6fcc5fac9522
tbukhantsova <tatyana.savenkova.82@mail.ru>
2024-08-20 19:52:22 +0300

    KS-2-1-DZ average mark

---
+++ src/main/kotlin/lesson_2_DZ/lesson_2_1.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_2_DZ
+
+fun main() {
+
+
+    val a: Float = 3f
+    val b: Float = 4f
+    val c: Float = 3f
+    val d: Float = 5f
+
+
+    val averageMark = (a + b + c + d) / 4
+
+    println(averageMark)
+
+  }
+
+
