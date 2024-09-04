673d3b5289dba54b5113e25c50723bd3797e85cd
tbukhantsova <tatyana.savenkova.82@mail.ru>
2024-09-04 12:47:02 +0300

    KS-2-4-DZ fix buff ore

--- src/main/kotlin/lesson_2/lesson_2_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_4.kt
@@ -2,17 +2,14 @@ package org.example.lesson_2
 
 fun main() {
 
-   // Инициализация переменных
-   val crystalOreWithoutBuff = 7 // количество кристаллической руды
-   val ironOreWithoutBuff = 11 // количество железной руды
-   val buffPercentage = 0.20 // 20% баффа
+   val crystalOreWithoutBuff = 7
+   val ironOreWithoutBuff = 11
+   val buffPercentage = 0.20
 
-   // Вычисление бонусных материалов с учетом баффа
-   val bonusCrystalOre = (crystalOreWithoutBuff * buffPercentage).toInt() // отбрасываем дробную часть
-   val bonusIronOre = (ironOreWithoutBuff * buffPercentage).toInt() // отбрасываем дробную часть
+   val bonusCrystalOre = (crystalOreWithoutBuff * buffPercentage).toInt()
+   val bonusIronOre = (ironOreWithoutBuff * buffPercentage).toInt()
 
-
-   println(bonusCrystalOre)
-   println(bonusIronOre)
+   println("BonusCrystalOre: $bonusCrystalOre")
+   println("BonusIronOre: $bonusIronOre")
 
 }
