7cb1e8d86893d90cbe7927c5f030333c5ccc03ca
tbukhantsova <tatyana.savenkova.82@mail.ru>
2024-09-04 12:33:06 +0300

    SK-3 Char in Kotlin Tanya

--- src/main/kotlin/SkillboxPractice/lesson_3.kt
+++ src/main/kotlin/SkillboxPractice/lesson_3.kt
@@ -2,12 +2,11 @@ package org.example.SkillboxPractice
 
 fun main () {
 
-    val letterCode1 = 75
-    val letterCode2 = 111
-    val letterCode3 = 116
-    val letterCode4 = 108
-    val letterCode5= 105
-    val letterCode6 = 110
+    val letterCode1 = 84
+    val letterCode2 = 97
+    val letterCode3 = 110
+    val letterCode4 = 121
+    val letterCode5 = 97
 
 
     val letter1: Char = Char(letterCode1)
@@ -15,8 +14,8 @@ fun main () {
     val letter3: Char = Char(letterCode3)
     val letter4: Char = Char(letterCode4)
     val letter5: Char = Char(letterCode5)
-    val letter6: Char = Char(letterCode6)
 
-    println("$letter1$letter2$letter3$letter4$letter5$letter6")
+
+    println("$letter1$letter2$letter3$letter4$letter5")
 
 }
